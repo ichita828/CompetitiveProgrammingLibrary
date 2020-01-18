e06a17052bc6cc722286f8ddbb4d640d441ee2a4
ichita828 <ishikzm0828@gmail.com>
2020-01-18 23:03:43 +0900

    remove

--- 数学.cs
+++ 数学.cs
@@ -1,30 +1,5 @@
 public class Math2
 {
-    public long[] fact;
-    public long[] invfact;
-    public void factoricalmodset(long N, long MOD = 1000000007)
-    {
-        fact = new long[N + 1];
-        invfact = new long[N + 1];
-        fact[0] = 1;
-
-        for (long i = 1; i <= N; ++i)
-        {
-            fact[i] = fact[i - 1] * i % MOD;
-        }
-        invfact[N] = pow(fact[N], MOD - 2);
-
-        for (long i = N; i > 0; --i)
-        {
-            invfact[i - 1] = invfact[i] * i % MOD;
-        }
-
-    }
-
-    public long nCr(long n, long r, long MOD = 1000000007)
-    {
-        return (fact[n] * invfact[r] % MOD * invfact[n - r] % MOD);
-    }
 
     public long pow(long i, long n, long MOD = 1000000007)
     {
