99b39ee07f0ee438606ad01b94caeeb58d94ecb1
ichita828 <ishikzm0828@gmail.com>
2020-01-20 21:14:07 +0900

    Add UnionFind

--- UnionFind.cs
+++ UnionFind.cs
@@ -1,4 +1,34 @@
-public class Unionfind
+public class UnionFind
 {
+    int[] par;
+    UnionFind(int n)
+    {
+        par = Enumerable.Range(0, n).Select(_ => -1).ToArray();
+    }
 
+    int root(int x)
+    {
+        if (par[x] < 0) return x;
+        else return par[x] = root(par[x]);
+    }
+
+    bool issame(int x, int y)
+    {
+        return root(x) == root(y);
+    }
+
+    bool merge(int x, int y)
+    {
+        x = root(x);
+        y = root(y);
+        if (x == y) return false;
+        if (par[x] > par[y]) Math2.swap(ref x, ref y);
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    int size(int x)
+    {
+        return -par[root(x)];
+    }
 }
