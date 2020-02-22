a177b882dad9aa8983e9bb77fb1821f9b650800a
ichita828 <ishikzm0828@gmail.com>
2020-02-22 15:19:10 +0900

    update

--- UnionFind.cs
+++ UnionFind.cs
@@ -1,34 +1,51 @@
 public class UnionFind
 {
-    int[] par;
+    int _count;
+    int[] par, sizes, rank;
     public UnionFind(int n)
     {
-        par = Enumerable.Range(0, n).Select(_ => -1).ToArray();
+        _count = n;
+        par = new int[n];
+        sizes = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; ++i)
+        {
+            par[i] = i;
+            sizes[i] = 1;
+            rank[i] = 0;
+        }
     }
-
     int root(int x)
     {
-        if (par[x] < 0) return x;
+        if (par[x] == x)
+        {
+            return x;
+        }
         else return par[x] = root(par[x]);
     }
 
-    bool issame(int x, int y)
-    {
-        return root(x) == root(y);
-    }
-
-    bool merge(int x, int y)
+    public bool merge(int x, int y)
     {
         x = root(x);
         y = root(y);
         if (x == y) return false;
-        if (par[x] > par[y]) Math2.swap(ref x, ref y);
-        par[x] += par[y];
-        par[y] = x;
+        --_count;
+        if (rank[x] < rank[y])
+        {
+            par[x] = y;
+            sizes[y] += sizes[x];
+        }
+        else
+        {
+            par[y] = x;
+            sizes[x] += sizes[y];
+            if (rank[x] == rank[y]) ++rank[x];
+        }
         return true;
     }
-    int size(int x)
-    {
-        return -par[root(x)];
-    }
+    public bool issame(int x, int y) => root(x) == root(y);
+    public int count => _count;
+    public bool ispar(int x) => x == root(x);
+    public int getpar(int x) => root(x);
+    public int size(int x) => sizes[root(x)];
 }
