e69e877a806287a18d311f830dda7db65a468d85
ichita828 <ishikzm0828@gmail.com>
2020-06-17 11:21:26 +0000

    fix

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -1,44 +1,126 @@
-public static class Graph {
-
-    public static long[] dijkstra(List<Tuple<long, int>>[] edge, int from, long INF = Math2.INFL) {
+public static class Graph
+{
+    public static long[] dijkstra(List<Tuple<long, int>>[] edge, int from, long INF = Math2.INFL)
+    {
         long[] ret = Enumerable.Range(0, edge.Length).Select(_ => INF).ToArray();
-        var pq = new PriorityQueue<Tuple<long, int>>((a, b) => (int) (a.Item1 - b.Item1));
+        var pq = new PriorityQueue<Tuple<long, int>>((a, b) => (int)(a.Item1 - b.Item1));
         ret[from] = 0;
         pq.Enqueue(new Tuple<long, int>(0, from));
-        while (pq.Count > 0) {
+        while (pq.Count > 0)
+        {
             var p = pq.Dequeue();
             var v = p.Item2;
             if (ret[v] < p.Item1) continue;
-            foreach (var e in edge[v]) {
-                if (ret[e.Item2] > ret[v] + e.Item1) {
+            foreach (var e in edge[v])
+            {
+                if (ret[e.Item2] > ret[v] + e.Item1)
+                {
                     ret[e.Item2] = ret[v] + e.Item1;
                     pq.Enqueue(new Tuple<long, int>(ret[e.Item2], e.Item2));
                 }
-
             }
         }
         return ret;
     }
+    public static long[][] Warshall_Floyd(List<Tuple<long, int>>[] edge)
+    {
+        var ret = new long[edge.Length][];
+        for (int i = 0; i < edge.Length; ++i)
+        {
+            ret[i] = Enumerable.Repeat(INFL, edge.Length).ToArray();
+        }
+        for (int i = 0; i < edge.Length; ++i)
+        {
+            foreach (var x in edge[i])
+            {
+                ret[i][x.Item2] = x.Item1;
+            }
+        }
+        for (int i = 0; i < edge.Length; ++i) ret[i][i] = 0;
+        for (int k = 0; k < edge.Length; ++k)
+        {
+            for (int i = 0; i < edge.Length; ++i)
+            {
+                for (int j = 0; j < edge.Length; ++j)
+                {
+                    ret[i][j] = Min(ret[i][j], ret[i][k] + ret[k][j]);
+                }
+            }
+        }
+        return ret;
 
-    public static bool IsBipartiteGraph(List<int>[] edge) {
-        var color = new int[edge.Length];
+    }
+    public static bool IsBipartiteGraph(List<Tuple<long, int>>[] edge)
+    {
+        var color = Enumerable.Repeat(-1, edge.Length).ToArray();
         Func<int, int, bool> dfs = null;
-        dfs = (int v, int c) => {
+        dfs = (int v, int c) =>
+        {
             color[v] = c;
-            foreach (var nv in edge[v]) {
-                if (color[nv] == c) return false;
-                if (color[nv] == -1 && !dfs(nv, 1 - c)) return false;
+            foreach (var nv in edge[v])
+            {
+                if (color[nv.Item2] == c) return false;
+                if (color[nv.Item2] == -1 && !dfs(nv.Item2, 1 - c)) return false;
             }
             return true;
         };
-
-        for (int i = 0; i < edge.Length; ++i) {
-            if (color[i] == 0) {
-                if (!dfs(i, 0)) {
+        for (int i = 0; i < edge.Length; ++i)
+        {
+            if (color[i] == -1)
+            {
+                if (!dfs(i, 0))
+                {
                     return false;
                 }
             }
         }
         return true;
     }
+    public static List<Tuple<long, int>>[] totp(List<int>[] lis)
+    {
+        var ret = new List<Tuple<long, int>>[lis.Length];
+        for (int i = 0; i < lis.Length; ++i)
+        {
+            ret[i] = new List<Tuple<long, int>>();
+            foreach (var x in lis[i])
+            {
+                ret[i].Add(new Tuple<long, int>(1, x));
+            }
+        }
+        return ret;
+    }
+    public static long[][] tomat(List<Tuple<long, int>>[] edge)
+    {
+        var ret = new long[edge.Length][];
+        for (int i = 0; i < edge.Length; ++i)
+        {
+            ret[i] = new long[edge.Length];
+            for (int j = 0; j < edge.Length; ++j)
+            {
+                ret[i][j] = Math2.INFL;
+            }
+        }
+        for (int i = 0; i < edge.Length; ++i)
+        {
+            foreach (var x in edge[i])
+            {
+                ret[i][x.Item1] = x.Item2;
+            }
+        }
+        return ret;
+    }
+    public static List<Tuple<long, int>>[] toli(long[][] s)
+    {
+        var ret = new List<Tuple<long, int>>[s.Length];
+        for (int i = 0; i < s.Length; ++i) ret[i] = new List<Tuple<long, int>>();
+        for (int i = 0; i < s.Length; ++i)
+        {
+            for (int j = 0; j < s.Length; ++j)
+            {
+                if (s[i][j] != 0) ret[i].Add(new Tuple<long, int>(s[i][j], j));
+            }
+        }
+        return ret;
+    }
+
 }
--- Segment/SegmentTree.cs
+++ Segment/SegmentTree.cs
@@ -1,34 +1,43 @@
-public class SegmentTree<T> {
+public class SegmentTree<T>
+{
     int N;
     T[] seg;
     T E;
     Func<T, T, T> F;
-    public SegmentTree(T[] arr, T e, Func<T, T, T> f) {
+    public SegmentTree(T[] arr, T e, Func<T, T, T> f)
+    {
         E = e;
         F = f;
         N = 1;
-        while (arr.Length > N)N <<= 1;
+        while (arr.Length > N) N <<= 1;
         seg = new T[2 * N - 1];
         Array.Fill(seg, E);
-        for (int i = 0; i < arr.Length; ++i) {
+        for (int i = 0; i < arr.Length; ++i)
+        {
             seg[i + N - 1] = arr[i];
         }
-        for (int i = N - 2; i >= 0; --i) {
+        for (int i = N - 2; i >= 0; --i)
+        {
             seg[i] = F(seg[2 * i + 1], seg[2 * i + 2]);
         }
     }
-    void Update(int k, T a) {
+    public SegmentTree(int n, T e, Func<T, T, T> f) : this(Enumerable.Repeat(e, n).ToArray(), e, f) { }
+
+    public void Update(int k, T a)
+    {
         k += N - 1;
         seg[k] = a;
-        while (k > 0) {
+        while (k > 0)
+        {
             k = (k - 1) / 2;
             seg[k] = F(seg[k * 2 + 1], seg[k * 2 + 2]);
         }
     }
-    public T Query(int a, int b, int k = 0, int l = 0, int r = -1) {
-        if (r < 0)r = N;
-        if (r <= a || b <= l)return E;
-        if (a <= l && r <= b)return seg[k];
+    public T Query(int a, int b, int k = 0, int l = 0, int r = -1)
+    {
+        if (r < 0) r = N;
+        if (r <= a || b <= l) return E;
+        if (a <= l && r <= b) return seg[k];
         int m = (l + r) / 2;
         return F(Query(a, b, k * 2 + 1, l, m), Query(a, b, k * 2 + 2, m, r));
     }
